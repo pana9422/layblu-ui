be6a2eac2c704e5c39d384e8f1d030d536626d49
Miguel J Medina <pana9422@gmail.com>
2022-06-21 18:56:52 -0500

    controller resizing editor-code and preview

--- src/components/theme/Code.js
+++ src/components/theme/Code.js
@@ -1,6 +1,4 @@
 import "./Code.css";
-
-import { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { faHtml5, faCss3, faJs } from "@fortawesome/free-brands-svg-icons";
 
@@ -33,16 +31,15 @@ const Code = () => {
         $prevEditorCode.style.height || setHeight($prevEditorCode, `${heightPrevEditorCode}px`)
         $prevEditorCode.style.height || setHeight($nextEditorCode, `${heightNextEditorCode}px`)
 
-        const handlerMouseMove = (event) => {
+
+        document.addEventListener('mousemove', (event) => {
             let prevHeightResizer = event.clientY - e.clientY + heightPrevEditorCode
             let nextHeightResizer = e.clientY - event.clientY + heightNextEditorCode
 
             setHeight($prevEditorCode, `${(prevHeightResizer)}px`)
             setHeight($nextEditorCode, `${(nextHeightResizer)}px`)
 
-        }
-
-        document.addEventListener('mousemove', handlerMouseMove, { signal })
+        }, { signal })
 
         document.addEventListener('mouseup', (e) => {
             controller.abort()
@@ -61,8 +58,9 @@ const Code = () => {
         let widthNextPreview = parseInt(window.getComputedStyle($nextPreview).width);
         
         $nextPreview.style.setProperty('min-width', `320px`)
-
         $nextPreview.classList.add("code__preview-resize")
+
+
         document.addEventListener('mousemove', (event) => {
             let prevWidthResizer = event.clientX - e.clientX + widthPrevEditorCode
             let nextWidthResizer = e.clientX - event.clientX + widthNextPreview
@@ -77,23 +75,64 @@ const Code = () => {
             $nextPreview.classList.remove("code__preview-resize")
         }, { signal })
     }
+
+    const handlerResizer = (size, e) => {
+
+
+        const controller = new AbortController()
+        const { signal } = controller
+
+        const $prevElement = e.target.previousElementSibling
+        const $nextElement = e.target.nextElementSibling
+
+        let sizePrevElement = parseInt(window.getComputedStyle($prevElement)[size]);
+        let sizeNextElement = parseInt(window.getComputedStyle($nextElement)[size]);
+
+        
+        size === "width" && $nextElement.style.setProperty('min-width', `320px`)
+        size === "width" && $nextElement.classList.add("code__preview-resize")
+
+        size === "width" || $prevElement.style.setProperty('min-height', `40px`)
+        size === "width" || $nextElement.style.setProperty('min-height', `40px`)
+
+        const addSize = (element, value) => element.style.setProperty(size, value)
+        
+        $prevElement.style[size] || addSize($prevElement, `${sizePrevElement}px`)
+        $nextElement.style[size] || addSize($nextElement, `${sizeNextElement}px`)
+
+        document.addEventListener('mousemove', (event) => {
+            let diffSizePrev = size === "width" ? event.clientX - e.clientX : event.clientY - e.clientY
+            let diffSizeNext = size === "width" ? e.clientX - event.clientX : e.clientY - event.clientY
+            let prevSizeResizer = diffSizePrev + sizePrevElement
+            let nextSizeResizer = diffSizeNext + sizeNextElement
+
+            $prevElement.style.setProperty(size, `${prevSizeResizer}px`)
+            $nextElement.style.setProperty(size, `${nextSizeResizer}px`)
+
+        }, { signal })
+
+        document.addEventListener('mouseup', () => {
+            controller.abort()
+            size === "width" && $nextElement.classList.remove("code__preview-resize")
+        }, { signal })
+    }
     return (
         <div className="code">
 
             <div className="code__editor">
                 <EditorCode content={html?.contentHTML} setContent={html?.setContentHTML} icon={faHtml5} lang="HTML" />
 
-                <div className="code__resizerH" onMouseDown={handlerMouseDownVertical}></div>
+                <div className="code__resizerH" onMouseDown={ (e) => handlerResizer('height', e) }></div>
 
                 <EditorCode content={css?.contentCSS} setContent={css?.setContentCSS} icon={faCss3} lang="CSS" />
 
-                <div className="code__resizerH" onMouseDown={handlerMouseDownVertical}></div>
+                <div className="code__resizerH" onMouseDown={ (e) => handlerResizer('height', e) }></div>
 
                 <EditorCode content={js?.contentJS} setContent={js?.setContentJS} icon={faJs} lang="JavaScript" />
 
             </div>
 
-            <div className="code__resizerV" onMouseDown={handlerMouseDownHorizontal}></div>
+            <div className="code__resizerV" onMouseDown={ (e) => handlerResizer('width', e) }></div>
 
             <iframe className="code__preview" title={code} srcDoc={preview} />
 
