34f829a1b96e514ce9f5973ce6130a263ae8f6fb
Miguel J Medina <pana9422@gmail.com>
2022-06-28 08:04:09 -0500

    order all components in data/components.js

--- src/data/components.js
+++ src/data/components.js
@@ -10,8 +10,7 @@ export const GROUP_COMPONENT = [
         slug: "accordions",
         max_width_item: 350,
         min_height_item: 500
-    },
-    {
+    },{
         name: "Alerts",
         slug: "alerts",
         max_width_item: 400,
@@ -42,8 +41,18 @@ export const GROUP_COMPONENT = [
         max_width_item: 350,
         min_height_item: 650
     }, {
+        name: "Inputs",
+        slug: "inputs",
+        max_width_item: 350,
+        min_height_item: 650
+    }, {
         name: "List Groups",
         slug: "list-groups",
+        max_width_item: 350,
+        min_height_item: 550
+    }, {
+        name: "Loading",
+        slug: "loading",
         max_width_item: 350,
         min_height_item: 550
     }, {
@@ -72,6 +81,11 @@ export const GROUP_COMPONENT = [
         max_width_item: 350,
         min_height_item: 250
     }, {
+        name: "Radio and Checkbox",
+        slug: "radio-and-checkbox",
+        max_width_item: 350,
+        min_height_item: 600
+    }, {
         name: "Selects",
         slug: "selects",
         max_width_item: 350,
@@ -86,11 +100,6 @@ export const GROUP_COMPONENT = [
         slug: "steppers",
         max_width_item: 450,
         min_height_item: 300
-    }, {
-        name: "Radio and Checkbox",
-        slug: "radio-and-checkbox",
-        max_width_item: 350,
-        min_height_item: 600
     }, {
         name: "Tables",
         slug: "tables",
@@ -115,24 +124,24 @@ export const GROUP_COMPONENT = [
 export const LIST_COMPONENTS = [
     {
         id: 1,
-        name: "Accordion collapsed",
-        path: "accordion-collapsed-1",
+        name: "Accordion",
+        path: "accordion-1",
         tags: GROUP_TAGS[1],
         group: GROUP_COMPONENT[0],
         author: DEVS[1]
     },
     {
         id: 2,
-        name: "Alert info",
-        path: "alert-info-2",
-        tags: GROUP_TAGS[0],
+        name: "Alert",
+        path: "alert-2",
+        tags: GROUP_TAGS[1],
         group: GROUP_COMPONENT[1],
         author: DEVS[0]
     },
     {
         id: 3,
-        name: "Avatar circle",
-        path: "avatar-circle-3",
+        name: "Avatar",
+        path: "avatar-3",
         tags: GROUP_TAGS[0],
         group: GROUP_COMPONENT[2],
         author: DEVS[0]
@@ -203,130 +212,162 @@ export const LIST_COMPONENTS = [
     },
     {
         id: 12,
-        name: "List articles",
-        path: "list-articles-12",
+        name: "Input placeholder",
+        path: "input-placeholder-12",
         tags: GROUP_TAGS[0],
         group: GROUP_COMPONENT[7],
         author: DEVS[0]
     },
     {
         id: 13,
-        name: "List items",
-        path: "list-items-13",
+        name: "Input floating",
+        path: "input-floating-13",
         tags: GROUP_TAGS[0],
         group: GROUP_COMPONENT[7],
         author: DEVS[0]
     },
     {
         id: 14,
-        name: "Modal",
-        path: "modal-14",
-        tags: GROUP_TAGS[1],
+        name: "List articles",
+        path: "list-articles-14",
+        tags: GROUP_TAGS[0],
         group: GROUP_COMPONENT[8],
-        author: DEVS[1]
+        author: DEVS[0]
     },
     {
         id: 15,
-        name: "Navbar",
-        path: "navbar-15",
-        tags: GROUP_TAGS[1],
-        group: GROUP_COMPONENT[9],
+        name: "List items",
+        path: "list-items-15",
+        tags: GROUP_TAGS[0],
+        group: GROUP_COMPONENT[8],
         author: DEVS[0]
     },
     {
         id: 16,
-        name: "Navigation",
-        path: "navigation-16",
-        tags: GROUP_TAGS[0],
-        group: GROUP_COMPONENT[10],
+        name: "Modal",
+        path: "modal-16",
+        tags: GROUP_TAGS[1],
+        group: GROUP_COMPONENT[9],
         author: DEVS[1]
     },
     {
         id: 17,
-        name: "Popover",
-        path: "popover-17",
+        name: "Navbar",
+        path: "navbar-17",
+        tags: GROUP_TAGS[1],
+        group: GROUP_COMPONENT[10],
+        author: DEVS[0]
+    },
+    {
+        id: 18,
+        name: "Navigation",
+        path: "navigation-18",
         tags: GROUP_TAGS[0],
         group: GROUP_COMPONENT[11],
         author: DEVS[1]
     },
     {
-        id: 18,
-        name: "Progress",
-        path: "progress-18",
-        tags: GROUP_TAGS[1],
+        id: 19,
+        name: "Popover",
+        path: "popover-19",
+        tags: GROUP_TAGS[0],
         group: GROUP_COMPONENT[12],
         author: DEVS[1]
     },
     {
-        id: 19,
-        name: "Select",
-        path: "select-19",
-        tags: GROUP_TAGS[1],
-        group: GROUP_COMPONENT[13],
-        author: DEVS[1]
-    },
-    {
         id: 20,
-        name: "Select Floating",
-        path: "select-floating-20",
+        name: "Progress",
+        path: "progress-20",
         tags: GROUP_TAGS[1],
         group: GROUP_COMPONENT[13],
         author: DEVS[1]
     },
     {
         id: 21,
-        name: "Slider",
-        path: "slider-21",
+        name: "Checkbox",
+        path: "checkbox-21",
         tags: GROUP_TAGS[1],
         group: GROUP_COMPONENT[14],
         author: DEVS[1]
     },
     {
         id: 22,
-        name: "Stepper",
-        path: "stepper-22",
+        name: "Switch",
+        path: "switch-22",
+        tags: GROUP_TAGS[1],
+        group: GROUP_COMPONENT[14],
+        author: DEVS[1]
+    },
+    {
+        id: 23,
+        name: "Select",
+        path: "select-23",
         tags: GROUP_TAGS[1],
         group: GROUP_COMPONENT[15],
         author: DEVS[1]
     },
     {
-        id: 23,
-        name: "Table",
-        path: "table-23",
-        tags: GROUP_TAGS[0],
-        group: GROUP_COMPONENT[16],
-        author: DEVS[1]
-    },
-    {
         id: 24,
-        name: "Tab horizontal",
-        path: "tab-horizontal-24",
+        name: "Select Floating",
+        path: "select-floating-24",
         tags: GROUP_TAGS[1],
-        group: GROUP_COMPONENT[17],
+        group: GROUP_COMPONENT[15],
         author: DEVS[1]
     },
     {
         id: 25,
-        name: "Tab vertical",
-        path: "tab-vertical-25",
+        name: "Slider",
+        path: "slider-25",
+        tags: GROUP_TAGS[1],
+        group: GROUP_COMPONENT[16],
+        author: DEVS[1]
+    },
+    {
+        id: 26,
+        name: "Stepper",
+        path: "stepper-26",
         tags: GROUP_TAGS[1],
         group: GROUP_COMPONENT[17],
         author: DEVS[1]
     },
     {
-        id: 26,
-        name: "Tag",
-        path: "tag-26",
+        id: 27,
+        name: "Table",
+        path: "table-27",
         tags: GROUP_TAGS[0],
         group: GROUP_COMPONENT[18],
+        author: DEVS[1]
+    },
+    {
+        id: 28,
+        name: "Tab horizontal",
+        path: "tab-horizontal-28",
+        tags: GROUP_TAGS[1],
+        group: GROUP_COMPONENT[19],
+        author: DEVS[1]
+    },
+    {
+        id: 29,
+        name: "Tab vertical",
+        path: "tab-vertical-29",
+        tags: GROUP_TAGS[1],
+        group: GROUP_COMPONENT[19],
+        author: DEVS[1]
+    },
+    {
+        id: 30,
+        name: "Tag",
+        path: "tag-30",
+        tags: GROUP_TAGS[0],
+        group: GROUP_COMPONENT[20],
         author: DEVS[0]
     },
     {
-        id: 27,
+        id: 31,
         name: "Timeline",
-        path: "timeline-27",
+        path: "timeline-31",
         tags: GROUP_TAGS[0],
-        group: GROUP_COMPONENT[19],
+        group: GROUP_COMPONENT[21],
         author: DEVS[1]
     }
 ]
